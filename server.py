55ad107b47887c6e8cd77b01d59f2474f5a8ac86
Dori <58356939+Betterslash@users.noreply.github.com>
2020-10-27 18:42:28 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -1,30 +1,54 @@
-import socket, pickle
-
-def sumOfElements(arr):
-    sum = 0
-    for x in arr:
-        sum += x
-    return pickle.dumps(sum)
-def countOfSpaces(strg):
-    count = 0
-    for s in strg:
-        if s == " ":
-            count += 1
-    return pickle.dumps(count)
+import socket
+import struct
+import threading
 
 HOST = '0.0.0.0'
-PORT = 3030
+PORT = 2020
+lock = threading.Lock()
+threads = []
+hangman = "HANGMAN"
+trueH = list("XXXXXXX")
+cuv = list("wordofcraft")
+dmy = list("___________")
+lives = 0
+
+def threadFunction(con):
+    global cuv, dmy, lives    
+    lgt = len(cuv)
+    lgt = struct.pack('!H', lgt)
+    con.send(lgt)
+    while True:
+        mes = ""
+        realH = ""
+        lit = con.recv(1)
+        lit = struct.unpack('!c', lit)[0].decode("utf-8")
+        print("I recived ", lit)
+        if lit not in cuv:
+            lives += 1
+            for i in range(lives):
+                trueH[i] = hangman[i] 
+        else:
+            for i in range(len(cuv)):
+                if cuv[i] == lit:
+                    dmy[i] = lit
+        for i in trueH:
+            realH += i 
+        for i in dmy:
+            mes += i
+        con.send(realH.encode())
+        con.send(mes.encode())
+    con.close()
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Socket created succesfully...")
+
 s.bind((HOST, PORT))
-s.listen(2)
-conn, addr = s.accept()
-print('Connected by')
-print(addr)
-while 1:
-    data = conn.recv(4096)
-    #if not data: break
-    print(pickle.loads(data))
-    msg = str(input("Text >>"))
-    data_text = pickle.dumps(msg)
-    conn.send(data_text)
-conn.close()
+print("Binded...")
+
+s.listen(5)
+while True:
+    conn, addr = s.accept()
+    print("Connection accepted...")
+    t = threading.Thread(target= threadFunction, args=(conn,))
+    threads.append(t)
+    t.start() 
